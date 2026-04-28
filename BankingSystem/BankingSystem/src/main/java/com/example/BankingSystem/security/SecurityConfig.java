d0d0e1ea1e224c30f93cbca64723af76c8783f5b
Rahul Rawat <rahulrawat84588@gmail.com>
2026-04-28 15:52:11 +0530

    fixed cors

--- BankingSystem/BankingSystem/src/main/java/com/example/BankingSystem/security/SecurityConfig.java
+++ BankingSystem/BankingSystem/src/main/java/com/example/BankingSystem/security/SecurityConfig.java
@@ -46,10 +46,7 @@ public CorsConfigurationSource corsConfigurationSource() {
 
     CorsConfiguration config = new CorsConfiguration();
 
-    config.setAllowedOrigins(List.of(
-        "http://localhost:5173",
-        "https://banking-system-ruddy-one.vercel.app"  // ✅ Real URL
-    ));
+    config.setAllowedOriginPatterns(List.of("*")); // ✅ changed this
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
